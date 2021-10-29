254a53b2cd1da0e0c5441a5b8a04463d7f0e59a3
Auricelio <auricelio.suporte@hotmail.com>
2021-10-28 21:13:37 -0300

    ORLANDO_CITY-Final

---
+++ ORLANDO_CITY/view/index.php
@@ -0,0 +1,140 @@
+<?php include_once("header.php");?>
+
+		<section>
+
+			<div id="banner">
+
+				<h1>Olando City <small>Olando City Soccer Club</small></h1>
+			
+			</div>
+
+		
+		<div id="news" class="container">
+			
+			<div class="row text-center">
+
+				<h2> Notícias </h2>
+				<hr>
+
+			</div>
+			
+			<button id="btn-news-prev" type="button"><i class="fa fa-angle-left"></i></button>
+			<button id="btn-news-next" type="button"><i class="fa fa-angle-right"></i></button>
+
+			<div class="row thumbnails">
+
+				<div class="col-md-3 item">
+					<a href="https://www.orlandocitysc.com/"><img src="img/news-thumb1.png" alt="thumb1"></a>
+					<h3>Pride Claim All Three Points in 3-1 Win Over Racing Louisville</h3>
+					<time>16 de setembro de 2021</time>						
+				</div>
+
+				<div class="col-md-3 item">
+					<a href="https://www.orlandocitysc.com/"><img src="img/news-thumb2.png" alt="thumb2"></a>
+					<h3>Lions Seven-Game Unbeaten Run Comes to an End With Loss at Atlanta</h3>
+					<time>17 de setembro de 2021</time>
+						
+				</div>
+				
+				<div class="col-md-3 item">
+					<a href="https://www.orlandocitysc.com/"><img src="img/news-thumb3.png" alt="thumb3"></a>
+					<h3>Orlando City SC Loans David Loera to Phoenix Rising FC</h3>
+					<time>18 de setembro de 2021</time>
+						
+				</div>
+
+				<div class="col-md-3 item">
+					<a href="https://www.orlandocitysc.com/"><img src="img/news-thumb4.png" alt="thumb4"></a>
+					<h3>Audi 2021 MLS Cup Playoffs Schedule Confirmed</h3>
+					<time>19 de setembro de 2021</time>						
+				</div>
+
+
+				<div class="col-md-3 item">
+					<a href="https://www.orlandocitysc.com/"><img src="img/news-thumb1.png" alt="thumb1"></a>
+					<h3>Pride Claim All Three Points in 3-1 Win Over Racing Louisville</h3>
+					<time>16 de setembro de 2021</time>						
+				</div>
+
+				<div class="col-md-3 item">
+					<a href="https://www.orlandocitysc.com/"><img src="img/news-thumb2.png" alt="thumb2"></a>
+					<h3>Lions Seven-Game Unbeaten Run Comes to an End With Loss at Atlanta</h3>
+					<time>17 de setembro de 2021</time>
+						
+				</div>
+				
+				<div class="col-md-3 item">
+					<a href="https://www.orlandocitysc.com/"><img src="img/news-thumb3.png" alt="thumb3"></a>
+					<h3>Orlando City SC Loans David Loera to Phoenix Rising FC</h3>
+					<time>18 de setembro de 2021</time>
+						
+				</div>
+
+				<div class="col-md-3 item">
+					<a href="https://www.orlandocitysc.com/"><img src="img/news-thumb4.png" alt="thumb4"></a>
+					<h3>Audi 2021 MLS Cup Playoffs Schedule Confirmed</h3>
+					<time>19 de setembro de 2021</time>						
+				</div>	
+
+			</div>
+
+		</div>
+
+		<div id="estatisticas">
+
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<p>61348 <small>Capacidade do Estádio</small></p>
+
+					</div>
+						
+					<div class="col-md-4">
+						<p>2010 <small>Fundado</small></p>
+					</div>
+
+					<div class="col-md-4">
+						<p>7º<small>Conferência Oriental</small></p>
+					</div>
+
+				</div>
+			</div>
+		</div>		
+
+		<div id="call-to-action">
+			
+			<div class="container">
+				
+				<div class="row text-center">
+					<h2>Clube Americano número um no Brasil</h2>
+					<hr>
+				</div>
+				
+				<div class="row">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+
+				<div id="btn" class="row">
+				
+					<div class="col-md-6">
+						<a href="https://www.orlandocitysc.com/" class="btn btn-roxo">Loja</a>
+					</div>	
+
+					<div class="col-md-6">
+						<a href="https://www.orlandocitysc.com/" class="btn btn-amarelo">Registrar</a>
+					</div>	
+
+				</div>
+			
+			</div>		
+		
+		</div>
+
+		</section>
+		
+<?php include_once("footer.php");?>		
