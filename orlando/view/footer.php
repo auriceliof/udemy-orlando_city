d336a92acff12a02d92ba1ca4c9f3cee2955d241
Auricelio <auricelio.suporte@hotmail.com>
2021-10-28 21:06:22 -0300

    ORLANDO-Final

---
+++ orlando/view/footer.php
@@ -0,0 +1,85 @@
+<footer>
+			<div class="row row-cinza-claro"> 
+
+				<div class="container">
+
+				<div class="row row-cols">
+
+					<div class="col-md-3">
+						<a href="index.php"><img id="logo" src="img/logo.png" alt="Logotipo"></a>
+					</div>
+						
+					<div id="populares" class="col-md-3">
+						<h4>POPULARES</h4>
+
+						<h5>Sites mais acessados</h5>
+						<time>21 de setembro de 2021</time>
+
+						<h5>Sites mais acessados</h5>
+						<time>21 de setembro de 2021</time>
+					</div>
+
+					<div id="links" class="col-md-3">
+						<h4>LINKS</h4>
+							<h6>
+								<a href="https://www.orlandocitysc.com/tickets/"><i class="fa fa-angle-rigth"></i>Ingressos</a>
+								<a href="https://www.orlandocitysc.com/news/"><i class="fa fa-angle-rigth"></i>Notícias</a>
+								<a href="https://www.orlandocitysc.com/schedule/"><i class="fa fa-angle-rigth"></i>Agenda</a>
+								<a href="entretenimento">Entretenimento</a>
+								<a href="loja">loja</a>
+							</h6>
+
+					</div>
+
+					<div class="col-md-3">
+						<h4>CONTATOS</h4>
+							<h6>
+								<address>
+									<i class="fa fa-map-marker"></i> 618 E. Street, Suite 510 Orlando, FL 32801
+								</address>
+
+								<p><a href="tel:1855ORLCITY"><i class="fa fa-phone"></i>1.855.ORL.CITY</a></p>
+								
+								<ul class="list-unstyled lista-sociais">
+									<li>
+										<a href="https://www.facebook.com/orlandocitysc" target="_blank"><i class="fa fa-facebook"></i></a>
+									</li>
+									<li>
+										<a href="https://www.twitter.com/orlandocitysc" target="_blank"><i class="fa fa-twitter"></i></a>
+									</li>
+									<li>
+										<a href="https://www.instagram.com/OrlandoCitySC/" target="_blank"><i class="fa fa-instagram"></i></a>
+									</li>
+									<li>
+										<a href="https://www.youtube.com/user/OrlandoCitySC" target="_blank"><i class="fa fa-youtube"></i></a>
+									</li>
+								</ul>
+
+							</h6>
+					</div>
+
+				</div>
+			</div>
+				
+			</div>
+
+			<div class="row row-cinza-escuro"> 
+
+				<div id="intelectual" class="container">
+					
+					<p id="copyrigth">©2021 MLS. The Major League Soccer and MLS name and shield are registered trademarks of Major League Soccer, L.L.C. (“MLS”).</p>
+					<p id="created">Created by Auricelio Freitas.</p>
+
+				</div>
+			</div>			
+			
+		</footer>	
+	
+		<script src="lib/jquery/jquery-min.js"></script>
+		<script src="lib/ows.carousel/owl-carousel/owl.carousel.min.js"></script>
+		<script src="lib/bootstrap/js/bootstrap.min.js"></script>
+		<script src="lib/raty/lib/jquery.raty.js"></script>
+		<script src="js/efeitos.js"></script>
+		
+	</body>
+</html>
